731c69177b9d0c7f74f0fd0e374ed54ccc91ebb2
Priyanshu Krishnan <krishnan.priyanshu@gmail.com>
2023-03-27 19:54:34 +0530

    add inpycon ci

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ const nextConfig = {
    
 }
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 module.exports = nextConfig
