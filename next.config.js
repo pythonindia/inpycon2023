2953699e47db921be1b728e8d2c70039875480ce
Priyanshu Krishnan <krishnan.priyanshu@gmail.com>
2023-03-27 20:13:34 +0530

    testing github action

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  basePath: '/gh-pages',
 }
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 
 module.exports = nextConfig
