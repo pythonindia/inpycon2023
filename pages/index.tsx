74b13b299a8857c41c533d626a7738f284bf5454
bhansa <bsaraswat45@gmail.com>
2023-04-03 18:27:49 +0530

    add switch for mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import PyConIndiaWeb from "./pycon-landing";
 import PyConIndiaMobile from "./pycon-mobile";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+type componentProps = {
+  isMobile: boolean;
+};
+
+export default function Home({ isMobile }: componentProps) {
   return (
     <div className="relative isolate px-6 pt-14 lg:px-8">
-      <PyConIndiaMobile />
+      {/* switch to show mobile layout for mobile, detection code below */}
+      {isMobile ? <PyConIndiaMobile /> : <PyConIndiaWeb />}
     </div>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const UA = context.req.headers["user-agent"];
+  const isMobile = Boolean(
+    UA.match(
+      /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+    )
+  );
+
+  return {
+    props: {
+      isMobile,
+    },
+  };
 }
