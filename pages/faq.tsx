39ff9a83a0fa24a973573eb570f299ca5722cbb3
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-05-12 22:42:17 +0530

    minor adjustment

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -11,7 +11,7 @@ const faqPage = () => {
     <div className='text-center'>
     <h1 className="text-5xl my-16">Frequently Asked Questions</h1>
     {faqs.map((faq, index) => (
-      <div key={index}>
+      <div key={index} className='my-5'>
         <h2 className='text-3xl'>{faq.question}</h2>
         <p>{faq.answer}</p>
         <hr/>
