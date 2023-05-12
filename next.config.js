79677e41c9f75beb1fadd4b41bcdeb5a024f2acf
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-05-12 22:25:22 +0530

    added faq page & its configs

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
-const nextConfig = {
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   return {
-  //     '/': { page: '/' },
-  //   }
-  // }
-  images: {
-    unoptimized: true
+module.exports = {
+  webpack: function (config) {
+    config.module.rules.push(
+      {
+        test: /\.ya?ml$/,
+        use: 'js-yaml-loader',
+      },
+    )
+    return config
   }
 }
-
-
-module.exports = nextConfig
---
+++ pages/faq.tsx
@@ -0,0 +1,24 @@
+import faqData from './faq.yaml'
+
+
+const faqPage = () => {
+    const faqs: Array<{
+        question: string;
+        answer: string;
+      }> = faqData;
+
+  return (
+    <div className='text-center'>
+    <h1 className="text-5xl my-16">Frequently Asked Questions</h1>
+    {faqs.map((faq, index) => (
+      <div key={index}>
+        <h2 className='text-3xl'>{faq.question}</h2>
+        <p>{faq.answer}</p>
+        <hr/>
+      </div>
+    ))}
+    </div>
+  );
+};
+
+export default faqPage;
