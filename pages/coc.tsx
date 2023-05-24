23d7b3f3b55da479513bfd4a8e2a20bd776f9ee5
targhs <34231252+targhs@users.noreply.github.com>
2023-05-25 00:03:07 +0530

    update content for code of conduct

--- pages/coc.tsx
+++ pages/coc.tsx
@@ -1,134 +1,212 @@
 export default function Coc() {
   return (
-    <div>
-      <h1>Code Of Conduct</h1>
-      <p>
-        PyCon India is a community organized conference intended for advocating
-        the use and adoption of the Python programming language in India. It is
-        also a platform for fostering networking and collaboration among the
-        Python developer community in India. We value the participation of every
-        member of the Python community and want all attendees to have an
-        enjoyable and rewarding experience. Accordingly, every attendee of the
-        conference is expected to show respect and courtesy to every other
-        attendee throughout the conference and at all conference related events,
-        whether officially organized by PyCon India or not. To make clear what
-        is expected, all delegates/attendees, speakers, exhibitors, organizers
-        and volunteers at PyCon India are required to conform to the following
-        Code of Conduct. Organizers will enforce this code throughout the event.
-      </p>
-      <h2>Short Version</h2>
-      <p>
-        PyCon India is dedicated to providing a harassment-free conference
-        experience for everyone, regardless of age, gender, sexual orientation,
-        physical appearance, disability, race, religion or employment. We don’t
-        tolerate harassment of attendees in any form. All communication should
-        be appropriate for a professional audience, including people from many
-        different backgrounds.
-      </p>
-      <h4>Code for Speakers:</h4>
-      <p>
-        Sexual language or imagery is inappropriate for your talks or slides.
-        Refrain from using sexist, racist or exclusionary language anywhere in
-        your content.
-      </p>
-      <h4>Code for Exhibitors and Sponsors:</h4>
-      <p>
-        Exhibitors in the expo hall, sponsor or vendor booths, are subject to
-        the anti-harassment policy. Exhibitors should not use sexualized images,
-        activities, or other material. Booth staff (including volunteers) should
-        not use sexualized clothing/uniforms/costumes, or otherwise create a
-        sexualized environment.
-      </p>
-      <h4>Code for Participants:</h4>
-      <p>
-        Be kind and sensitive to the people around you and avoid any kind of
-        offensive behavior. Sexist, racist or any other form of exclusionary or
-        offensive jokes or excessive public swearing are not appropriate at any
-        venue of PyCon India. Attendees violating these rules may be asked to
-        leave the conference without a refund at the sole discretion of the
-        conference organizers. Thank you for your consideration and help in
-        making PyCon India a welcoming, friendly event for all of us.
-      </p>
-      <h2>Long Version</h2>
-      <p>
-        PyCon India is dedicated to providing a harassment-free conference
-        experience for everyone, regardless of age, gender, sexual orientation,
-        physical appearance, disability, race, religion or employment.
-      </p>
-      <p>
-        Harassment includes offensive verbal comments related to gender, sexual
-        orientation, disability, physical appearance, body size, race, religion,
-        sexual images in public spaces, deliberate intimidation, stalking,
-        following, harassing photography or recording, sustained disruption of
-        talks or other events, inappropriate physical contact, and unwelcome
-        sexual attention. We have zero tolerance on harassment of conference
-        participants in any form, including, but not limited to the activities
-        mentioned here.
-      </p>
-      <p>
-        Participants asked to stop any harassing behavior are expected to comply
-        immediately.
-      </p>
-      <p>
-        Exhibitors in the expo hall, sponsor or vendor booths, or similar
-        activities are also subject to the anti-harassment policy. In
-        particular, exhibitors should not use sexualized images, activities, or
-        other material. Booth staff (including volunteers) should not use
-        sexualized clothing/uniforms/costumes, or otherwise create a sexualized
-        environment.
-      </p>
-      <p>
-        All communication should be appropriate for a professional audience,
-        including people from many different backgrounds. Sexual language or
-        imagery is inappropriate for all aspects of the conference, including
-        talks. Remember that sexist, racist or any other form of exclusionary or
-        offensive jokes or excessive public swearing are not appropriate at any
-        venue of PyCon India.
-      </p>
-      <p>
-        Do not insult or put down attendees or engage in any action that
-        violates the open, welcoming and sharing spirit of the conference. Be
-        kind and sensitive to the people around you when you are attending the
-        conference, and avoid any kind of offensive or degrading behavior.
-      </p>
-      <p>
-        If a participant engages in behavior that violates this code of conduct,
-        the conference organizers may take any action they deem appropriate,
-        including warning the offender or expulsion from the conference with no
-        refund.
-      </p>
-      <p>
-        Thank you for helping to make PyCon India a welcoming, friendly event
-        for all.
-      </p>
-      <h4>Contact Information</h4>
-      <p>
-        If you are being harassed, notice that someone else is being harassed,
-        or have any other concerns, please contact an Organizer or drop an email
-        to <a href="mailto:report@in.pycon.org">report@in.pycon.org</a>. Read
-        more about it in our{" "}
-        <a href="https://in.pycon.org/2023/reporting/">Reporting Guide</a>
-      </p>
-      <p>
-        If the matter is especially urgent, please call/contact any of these
-        individuals:
-      </p>
-      <ul>
-        <li>
-          Dummy Contact Name
-          <ul>
-            <li>Code of Conduct WG</li>
-            <li>dummy_email@pycon.com</li>
+    <div className="mx-3">
+      <p className="text-4xl font-extrabold">Code Of Conduct</p>
+
+      <div className="my-6">
+        <p className="text-2xl font-semibold">The Short Form</p>
+        <p className="text-base my-2">
+          We invite you to come to PyCon India 2023 in a spirit of curiosity,
+          friendliness, open-mindedness, and respect. We will not tolerate
+          harassment in any form.
+        </p>
+        <p className="text-base my-2">
+          At the PyCon India venue and related online forums, you agree to:
+        </p>
+        <ul className="list-disc list-inside my-2">
+          <li>
+            Be kind and sensitive to the people around you and respect the
+            boundaries of other attendees.
+          </li>
+          <li>Attempt to resolve difference of opinions peacefully</li>
+          <li>
+            Avoid harassment, exclusionary or offensive behaviour of any kind.
+            This includes but is not limited to offensive verbal comments
+            related to gender, sexual orientation, disability, physical
+            appearance, body size, race, religion, sexual images in public
+            spaces, deliberate intimidation, stalking, following, harassing
+            photography or recording, sustained disruption of talks or other
+            events, inappropriate physical contact, and unwelcome sexual
+            attention.
+          </li>
+        </ul>
+        <p className="text-base my-2">
+          Violations can be reported online by sending a mail to
+          <a
+            href="mailto:balchandani.sanchit@gmail.com"
+            className="text-blue-600 visited:text-purple-600"
+          >
+            {" "}
+            balchandani.sanchit@gmail.com
+          </a>
+          , or to PyCon India volunteers at the venue. You can also report
+          directly to the Conference Chair, Sanchit Balchandani. He can be
+          reached at the mobile no. +91-98998-38120. Please read our
+          <a href="#" className="text-blue-600 visited:text-purple-600">
+            {" "}
+            Reporting Guide
+          </a>{" "}
+          for reporting guidelines and resolution procedure.
+        </p>
+        <p className="text-base my-2">
+          Violations of the Code of Conduct can result in expulsion from the
+          conference with no refund. Please read on for a more
+          <a href="#" className="text-blue-600 visited:text-purple-600">
+            {" "}
+            detailed version of the Code of Conduct
+          </a>
+          .
+        </p>
+      </div>
+
+      <div className="my-6">
+        <p className="text-2xl font-semibold my-2">The Long Form</p>
+
+        <p className="text-lg font-semibold">Aim</p>
+        <p className="text-base my-2">
+          PyCon India is a community organized conference intended for
+          advocating the use and adoption of the Python programming language in
+          India. It is also a platform for fostering networking and
+          collaboration among the Python developer community in India. We
+          believe in the broad spectrum of individual and collective experience
+          and in the inherent dignity of all people. We believe that amazing
+          things happen when people from different worlds and world-views
+          approach each other to create a conversation. PyCon India 2023
+          welcomes everyone and is dedicated to providing a harassment-free
+          conference experience for everyone, regardless of age, gender, sexual
+          orientation, physical appearance, disability, neurotype, race,
+          religion, political opinion, culture or employment. We don’t tolerate
+          harassment of attendees in any form.
+        </p>
+
+        <p className="text-lg font-semibold my-2">Where</p>
+        <p className="text-base my-2">
+          his Code of Conduct applies to the venue of PyCon India 2023
+          Conference, pre-event & post-event informal gatherings, related online
+          forums and all conference related events organized by PyCon India.
+        </p>
+
+        <p className="text-lg font-semibold my-2">Who</p>
+        <p className="text-base my-2">
+          This is applicable to speakers, participants, sponsors, volunteers and
+          organizers.
+        </p>
+
+        <p className="text-lg font-semibold my-2">Be The Best You Can Be</p>
+        <ul className="list-disc list-inside my-2">
+          <li>
+            Be kind, sensitive, understanding and respectful to other attendees.
+          </li>
+          <li>Listen with empathy when someone has a different perspective.</li>
+          <li>
+            Talk to the volunteers if you notice that something could be better.
+          </li>
+          <li>
+            All communication should be appropriate for a professional audience,
+            including people from many different backgrounds.
+          </li>
+        </ul>
+
+        <p className="text-lg font-semibold my-2">Please Refrain From</p>
+        <ul className="list-disc list-inside my-2">
+          <li>
+            Offensive verbal comments related to gender, sexual orientation,
+            disability, physical appearance, body size, race, religion
+          </li>
+          <li>Violent threats or language directed against another person.</li>
+          <li>
+            Swearing, insults or discriminatory/exclusionary/offensive jokes or
+            language.
+          </li>
+          <li>
+            Deliberate intimidation, stalking, following, harassing photography
+            or recording
+          </li>
+          <li>Sustained disruption of talks or other events</li>
+          <li>
+            Using sexually explicit or violent material in presentations. Sexual
+            language or imagery is inappropriate for all aspects of the
+            conference, including talks and exhibits.
+          </li>
+          <li>Unwelcome sexual attention or inappropriate physical contact.</li>
+          <li>Advocating for, or encouraging, any of the above behavior.</li>
+          <li>
+            Repeated harassment of others. In general, if someone asks you to
+            stop, then stop.
+          </li>
+          <li>
+            Avoid use of sexualized clothing/uniforms/costumes, or otherwise
+            create a sexualized environment.
+          </li>
+        </ul>
+        <p className="text-base my-1">
+          We have zero tolerance on harassment of conference participants in any
+          form, including, but not limited to the activities mentioned here.
+        </p>
+        <p className="text-base my-1">
+          Participants asked to stop any harassing behavior are expected to
+          comply immediately.
+        </p>
+      </div>
+
+      <div className="my-6">
+        <p className="text-2xl font-semibold my-2">Reporting</p>
+        <div>
+          <p className="text-base my-1">
+            If you are being harassed, notice that someone else is being
+            harassed, or have any other concerns, please contact a conference
+            volunteer. Volunteers will be wearing “PyCon India Volunteer”
+            badges. For detailed instructions on reporting of violations and the
+            steps for resolution, please check the
+            <a href="/2023/reporting-guide" className="text-blue-600 visited:text-purple-600">
+              {" "}
+              Reporting Guide.
+            </a>
+          </p>
+          <p className="text-base my-1">
+            If the matter is especially urgent, you may also ask to be put in
+            touch with the Conference Chair, Sanchit Balchandani.
+          </p>
+          <p className="text-base my-1">
+            In case you are not able to locate a conference volunteer or you
+            would prefer to report directly to the Conference Chair, he can be
+            reached at the mobile no. +91-98998-38120 and the email address
+            <a
+              href="mailto:balchandani.sanchit@gmail.com"
+              className="text-blue-600 visited:text-purple-600"
+            >
+              {" "}
+              balchandani.sanchit@gmail.com
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+
+      <div className="my-6">
+        <p className="text-2xl font-semibold my-2">License</p>
+        <div className="my-2">
+          <p className="text-base">This Code of Conduct is derived from</p>
+          <ul className="list-disc list-inside mt-2">
+            <li>
+              PyCon India 2019 Code of Conduct which is under a Creative Commons
+              Attribution 3.0 Unported License.
+            </li>
+            <li>
+              The Django Code of Conduct by the Django Software Foundation which
+              is under a Creative Commons Attribution 3.0 Unported License
+            </li>
+            <li>
+              The PSF Code of Conduct by Python Software Foundation which is
+              under a Creative Commons Attribution 3.0 Unported License
+            </li>
           </ul>
-        </li>
-      </ul>
-      <h4 id="license">License</h4>
-      <p>
-        This Code of Conduct was forked from PSF Code of Conduct by Python
-        Software Foundation which is under a Creative Commons Attribution 3.0
-        Unported License. PyCon India Conference Code of Conduct is licensed
-        under a Creative Commons Attribution 3.0 Unported License.
-      </p>
+          <p className="text-base my-2">
+            PyCon India Conference Code of Conduct is licensed under a Creative
+            Commons Attribution 3.0 Unported License.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
