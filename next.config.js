b7bfdc3a1a17a155c97d794ec0691541a108419d
targhs <34231252+targhs@users.noreply.github.com>
2023-05-25 00:03:45 +0530

    update code of conduct route

--- next.config.js
+++ next.config.js
@@ -12,10 +12,17 @@ const nextConfig = {
   //     '/': { page: '/' },
   //   }
   // }
+  rewrites: async () => {
+    return [
+      {
+        source: "/2023/code-of-conduct",
+        destination: "/coc",
+      },
+    ];
+  },
   images: {
     unoptimized: true
   }
 }
-
 
 module.exports = nextConfig
