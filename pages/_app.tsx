2913633d388f73a5388e8f4a7c99aad2ecb26285
bhansa <bsaraswat45@gmail.com>
2023-06-07 00:08:40 +0530

    remove unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,12 @@
 import { AppProps } from "next/app";
-import '../styles/css/bootstrap.min.css';
-import '../styles/css/font-awesome-min.css';
-import '../styles/css/owl.carousel.css';
-import '../styles/css/owl.theme.default.min.css';
-import '../styles/css/style.css';
-import '../styles/css/aos.css';
+import "../styles/css/bootstrap.min.css";
+import "../styles/css/font-awesome-min.css";
+import "../styles/css/owl.carousel.css";
+import "../styles/css/owl.theme.default.min.css";
+import "../styles/css/style.css";
+import "../styles/css/aos.css";
 
-import dynamic from "next/dynamic";
-
-import "@fortawesome/fontawesome-svg-core/styles.css";
-
-import { config } from "@fortawesome/fontawesome-svg-core";
 import Head from "next/head";
-// Tell Font Awesome to skip adding the CSS automatically
-// since it's already imported above
-config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
