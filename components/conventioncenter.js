2f16ae68a232642264a7e6f63125e2c7c3f73890
Priyanshu Krishnan <krishnan.priyanshu@gmail.com>
2023-06-06 05:55:56 +0530

    add all sections into the component folder

---
+++ components/conventioncenter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ConventionCenterSection = () => {
+  return (
+    <section className="bg-hall d-flex align-items-center">
+      <div className="container">
+        <div className="row pt-5 pb-5 align-items-center">
+          <div className="col-md-8 text-white text-center text-md-start">
+            <h3 className="text-white hallhead">
+              Hyderabad International <br /> Convention Center
+            </h3>
+            <p className="text-white">
+              Novotel and HICC Complex, Kondapur, Hyderabad, Telangana 500081
+            </p>
+          </div>
+          <div className="col-md-4 text-center text-md-start">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.731859931853!2d78.3710477239269!3d17.472543450348773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb922dedfac813%3A0xe3675c0e78567be0!2sHYDERABAD%20INTERNATIONAL%20CONVENTION%20CENTRE-HICC%2C%20Izzathnagar%2C%20Hyderabad%2C%20Telangana%20500084!5e0!3m2!1sen!2sin!4v1685686414899!5m2!1sen!2sin"
+              height={400}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ConventionCenterSection;
--- components/journey.js
+++ components/journey.js
@@ -1,65 +1,86 @@
-import JourneyData from '../data/journey.yml';
+import { Carousel, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import JourneyData from "../data/journey.yml";
 
 const JourneyComponent = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? JourneyData.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setIndex((prevIndex) =>
+      prevIndex === JourneyData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % JourneyData.length);
+    }, 10000); // Set the desired autoplay interval in milliseconds (e.g., 3000 for 3 seconds)
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="bg-journey" id="journey">
       <div className="container">
         <div className="row pt-5">
           <div className="col-md-12 pt-3 pb-5">
             <h2 className="com-head">Journey</h2>
-          </div>       
-          <div id="carouselExampleAutoplaying" className="carousel slide " data-bs-ride="carousel">
-            <div className="carousel-inner">
-              {JourneyData.map((journey, index) => (
-                <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                  <div className="slide-1 p-3">
-                    <div className="row align-items-center justify-content-around">
-                      <div className="col-md-5">
-                        <div className="s1-image">
-                          <img src={journey.image} className="img-fluid w-100" alt="" />
-                        </div>
+          </div>
+          <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+            {JourneyData.map((journey, index) => (
+              <Carousel.Item key={index}>
+                <div className="slide-1 p-3">
+                  <div className="row align-items-center justify-content-around">
+                    <div className="col-md-5">
+                      <div className="s1-image">
+                        <img
+                          src={journey.image}
+                          className="img-fluid w-100"
+                          alt=""
+                        />
                       </div>
-                      <div className="col-md-7">
-                        <div className="s1-text p-2">
-                          <h2 className="mb-3">
-                            {journey.title} {journey.year}
-                          </h2>
-                          <div className="venue d-flex mt-4 justify-content-between">
-                            <div className="date">
-                              <h6 className="fw-bolder">Date:</h6>
-                              <p>{journey.date}</p>
-                            </div>
-                            <div className="place">
-                              <h6 className="fw-bolder">Location:</h6>
-                              <p>{journey.location}</p>
-                            </div>
+                    </div>
+                    <div className="col-md-7">
+                      <div className="s1-text p-2">
+                        <h2 className="mb-3">
+                          {journey.title} {journey.year}
+                        </h2>
+                        <div className="venue d-flex mt-4 justify-content-between">
+                          <div className="date">
+                            <h6 className="fw-bolder">Date:</h6>
+                            <p>{journey.date}</p>
                           </div>
-                          <h6 className="fw-bolder mt-4">
-                            Keynote Speaker: {journey.speaker}
-                          </h6>
-                          <p>
-                            {journey.description}
-                          </p>
+                          <div className="place">
+                            <h6 className="fw-bolder">Location:</h6>
+                            <p>{journey.location}</p>
+                          </div>
                         </div>
+                        <h6 className="fw-bolder mt-4">
+                          Keynote Speaker: {journey.speaker}
+                        </h6>
+                        <p>{journey.description}</p>
                       </div>
                     </div>
                   </div>
                 </div>
-              ))}
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true" />
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true" />
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>          
-        </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </div>  
       </div>
-      <div className="container-fluid p-0"> 
-        <div className="pt-5"><img src='images/journey.png' className="img-fluid" /></div>
+      <div className="container-fluid p-0">
+        <div className="pt-5">
+          <img src="images/journey.png" className="img-fluid" />
+        </div>
       </div>
     </section>
   );
