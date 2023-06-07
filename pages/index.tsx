2e8dd4bf6778a3739a02b053bff5571e82bc50a1
bhansa <bsaraswat45@gmail.com>
2023-06-08 00:06:39 +0530

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-import { useMediaQuery } from "@mui/material";
-import { Inter } from "next/font/google";
 import PyConIndiaWeb from "./pycon-landing";
-
-const inter = Inter({ subsets: ["latin"] });
 
 type componentProps = {
   isMobile: boolean;
 };
 
 export default function Home() {
-  const isMobile = useMediaQuery("(max-width:600px)");
   return <PyConIndiaWeb />;
 }
