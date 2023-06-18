83e9a6ef5b8c55d231af2071272a123e99c6315c
Rajat Rajdeep <rajat.rajdeep@avasant.com>
2023-06-18 22:21:30 +0530

    Fixes 4 functional issues.

--- components/header.js
+++ components/header.js
@@ -1,25 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import logo from "../public/images/logo.png";
+
 export default function Header() {
-  function handleClick() {
-    let navDrop = document.getElementById("navbarNavDropdown");
-    let hamburger = document.getElementsByClassName("navbar-toggler")[0];
-    // TODO
-    if (
-      navDrop.classList[0] == "collapse" ||
-      navDrop.classList[1] == "collapse"
-    ) {
-      navDrop.classList.remove("collapse");
-      hamburger.setAttribute("aria-expanded", "true");
-    } else {
-      navDrop.classList.add("collapse");
-      hamburger.setAttribute("aria-expanded", "false");
+  const [activeNavBarItem, setActiveNavBarItem] = useState(0);
+  const [navBarToggle, setNavBarToggle] = useState(false);
+  const navBarItems = [
+    {
+      name: "Home",
+      href: "/",
+      openInNewTab: false,
+    },
+    {
+      name: "Journey",
+      href: "/#journey",
+      openInNewTab: false,
+    },
+    {
+      name: "Attend",
+      href: "/#attend",
+      openInNewTab: false,
+    },
+    {
+      name: "Sponsors",
+      href: "/#sponsors",
+      openInNewTab: false,
+    },
+    {
+      name: "Blog",
+      href: "https://in.pycon.org/blog/",
+      openInNewTab: true,
+    },
+    {
+      name: "FAQ",
+      href: "/faq",
+      openInNewTab: false,
+    },
+    {
+      name: "COC",
+      href: "/code-of-conduct",
+      openInNewTab: false,
     }
-    // TODO
-  }
+  ]
 
   return (
     <header className="bg-header sticky-top">
@@ -46,29 +73,29 @@ export default function Header() {
                   data-bs-toggle="collapse"
                   data-bs-target="#navbarNavDropdown"
                   aria-controls="navbarNavDropdown"
-                  aria-expanded="true"
+                  aria-expanded={navBarToggle ? "true" : "false"}
                   aria-label="Toggle navigation"
-                  onClick={handleClick}
+                  onClick={() => setNavBarToggle((prv) => (!prv))}
                 >
                   <FontAwesomeIcon icon={faBars} />
                 </button>
                 <div
-                  className="collapse navbar-collapse"
+                  className={"navbar-collapse" + (navBarToggle ? "" : " collapse")}
                   id="navbarNavDropdown"
                 >
                   <ul className="navbar-nav">
-                    <li className="nav-item">
-                      <Link href="/"><span className="nav-link active">Home</span></Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/#journey"><span className="nav-link">Journey</span></Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/#attend"><span className="nav-link">Attend</span></Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/#sponsors"><span className="nav-link">Sponsors</span></Link>
-                    </li>
+                    {navBarItems.map((item, index) => (
+                      <li key={index} className="nav-item">
+                        <Link
+                          href={item.href}
+                          target={item.openInNewTab ? "_blank" : "_self"}
+                          onClick={() => setNavBarToggle(false)}
+                        >
+                          <span className={"nav-link" + (index == activeNavBarItem ? " active" : "")}>
+                            {item.name}</span>
+                        </Link>
+                      </li>
+                    ))}
                     {/* <li className="nav-item">
                       <Link href="/#keynote"><span className="nav-link">Keynotes</span></Link>
                     </li> */}
@@ -78,17 +105,6 @@ export default function Header() {
                     {/* <li className="nav-item">
                       <Link href="/#events"><span className="nav-link">Events</span></Link>
                     </li> */}
-                    <li className="nav-item">
-                      <Link href="https://in.pycon.org/blog/" passHref>
-                        <span className="nav-link" target="_blank">Blog</span>
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/faq"><span className="nav-link">FAQ</span></Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="/code-of-conduct"><span className="nav-link">COC</span></Link>
-                    </li>
                     {/* <li className="nav-item dropdown">
                       <span
                         className="nav-link dropdown-toggle"
