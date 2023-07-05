a38bce48fc93d95fc69bcfff7ec09ea7a1cbebda
Rajat Rajdeep <46029666+RajatRajdeep@users.noreply.github.com>
2023-07-05 20:06:20 +0530

    404 Page (#100)

---
+++ pages/404.tsx
@@ -0,0 +1,64 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useEffect, useRef } from "react";
+
+import lottie from "lottie-web";
+
+import Button from "../components/button";
+import animationData from "../public/images/lottie/snake-animation.json";
+
+const NotFoundPage = () => {
+  const router = useRouter();
+  const animationContainer = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (animationContainer.current) {
+      var animation = lottie.loadAnimation({
+        container: animationContainer.current,
+        animationData: animationData,
+        loop: true,
+        autoplay: true,
+      });
+    }
+
+    return () => {
+      animation.destroy();
+    };
+  }, []);
+
+  const goBack = () => {
+    router.back();
+  };
+
+  return (
+    <>
+      <Head>
+        <title>404 - Page Not Found</title>
+      </Head>
+      <div className="bg-404">
+        <h2 className="com-head text-center pt-5">Oops!</h2>
+        <h3 className="date-text text-center">
+          We couldn&rsquo;t find the page you were looking for.
+        </h3>
+        <div className="animation-container" ref={animationContainer} />
+        <div className="d-flex justify-content-center">
+          <Button
+            buttonClassName="submit-btn register-btn-extra-padding m-2"
+            anchorClassName="text-decoration-none text-light"
+            buttonLabel="Go Back"
+            onClickEvent={goBack}
+          />
+          <Button
+            buttonClassName="green-btn register-btn-extra-padding m-2"
+            anchorClassName="text-decoration-none"
+            buttonLabel="Go to Home"
+            openInNewTab={false}
+            buttonHyperLink="/2023/"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
