742a73d8fe9b65e5910dbfadb148126fd18cdc57
Rajat Rajdeep <rajatrajdeep@outlook.com>
2023-08-30 12:03:38 +0530

    Revert faq and coc nav item activation fix

--- components/coc.js
+++ components/coc.js
@@ -2,15 +2,16 @@ import ReactMarkdown from "react-markdown";
 
 export default function CoC({ markdownContent }) {
   return (
-    <div id="coc">
+    <>
       <h1
         className="com-head text-center"
         data-aos="fade-down"
         data-aos-duration="800"
+        id="coc"
       >
         Code of Conduct
       </h1>
       <ReactMarkdown className="content">{markdownContent}</ReactMarkdown>
-    </div>
+    </>
   );
 }
--- components/faqs.js
+++ components/faqs.js
@@ -2,15 +2,16 @@ import ReactMarkdown from "react-markdown";
 
 export default function Faqs({ markdownContent }) {
   return (
-    <div id="faq">
+    <>
       <h1
         className="com-head text-center"
         data-aos="fade-down"
         data-aos-duration="800"
+        id="faq"
       >
         Frequently Asked Questions
       </h1>
       <ReactMarkdown className="content">{markdownContent}</ReactMarkdown>
-    </div>
+    </>
   );
 }
