9058cae995f28ca1a2c17fccf6f7b345258b4190
Rajat Rajdeep <rajatrajdeep@outlook.com>
2023-09-01 17:48:50 +0530

    Improve accessibility features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <html lang="en" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
