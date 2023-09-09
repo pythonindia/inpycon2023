f82c8566e458981a206c565bd56bead9a6c1e64e
Rajat Rajdeep <rajatrajdeep@outlook.com>
2023-09-09 15:06:37 +0530

    Download Prospectus style modification

--- components/sponsor.js
+++ components/sponsor.js
@@ -28,7 +28,7 @@ const SponsorComponent = () => {
           </div>
           <div className="col-md-4 pb-md-0 pb-5 text-center">
             <Button
-              buttonClassName="custom-button green-btn mx-auto"
+              buttonClassName="custom-button green-btn"
               anchorClassName="text-decoration-none text-light"
               buttonLabel="Download Prospectus"
               buttonHyperLink="https://drive.google.com/file/d/1rIb3PLx9gUi-oWPdOdmldbHLy8Vaz-E2/view?usp=sharing"
