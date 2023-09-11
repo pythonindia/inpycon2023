1f49db5f05319b1101fa6645ce693ef0d7875f84
Bharat Saraswat <bsaraswat45@gmail.com>
2023-09-12 02:22:15 +0530

    WIP Feature/schedule design (#189)
    * wip schedule changes
    
    * wip schedule section changes
    
    * add mobile accordion changes, update yml data formatting
    
    * fix key prop
    
    * Add schedule of PyCon India 2023
    
    ---------
    
    Co-authored-by: Sanchit Balchandani <balchandani.sanchit@gmail.com>

--- components/schedule.js
+++ components/schedule.js
@@ -1,5 +1,9 @@
+import { Accordion, Card } from "react-bootstrap";
 import ScheduleData from "../data/schedule.yml";
 import React, { useState } from "react";
+
+// group by array using a condition, param 1 -> array, param 2 -> function for filtering and grouping
+const groupBy = (a, f) => a.reduce((x, c) => (x[f(c)] ??= []).push(c) && x, {});
 
 const ConferenceSchedule = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -20,8 +24,12 @@ const ConferenceSchedule = () => {
               data-aos="fade-down"
               data-aos-duration="1000"
             >
-              Conference Schedule{" "}
-              <img src="/2023/images/icons/sandwatch.png" alt="" className="img-fluid" />
+              Conference Schedule
+              <img
+                src="/2023/images/icons/sandwatch.png"
+                alt="sandwatch"
+                className="img-fluid"
+              />
             </h2>
             <ul
               className="nav nav-pills mb-3 pt-5"
@@ -57,35 +65,127 @@ const ConferenceSchedule = () => {
                 role="tabpanel"
                 aria-labelledby="pills-home-tab"
               >
-                {currentSchedule.schedule.map((scheduleItem, idx) => (
-                  <div
-                    className="row bt-bottom align-items-center pt-4 pb-4"
-                    key={idx}
-                  >
-                    <div className="col-md-3">
-                      <p className="mb-0 date-announced">{scheduleItem.time}</p>
-                    </div>
-                    <div className="col-md-2 text-center">
-                      <img
-                        src={scheduleItem.image}
-                        className="img-fluid"
-                        alt=""
-                      />
-                    </div>
-                    <div className="col-md-7">
-                      <p className="mb-0 date-content">
-                        {scheduleItem.description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
+                <div className="row">
+                  {currentSchedule.schedule.map((scheduleItem, idx) => {
+                    const { time, image, description, speaker, track } =
+                      scheduleItem;
+                    return <ScheduleCard {...scheduleItem} key={idx} />;
+                  })}
+                </div>
               </div>
             </div>
+
+            {/* Mobile Accordion  */}
+            {ScheduleData.map((item, idx) => {
+              return (
+                <ScheduleAccordion
+                  key={idx}
+                  id={idx}
+                  currentSchedule={currentSchedule}
+                  handleTabClick={() => handleTabClick(idx)}
+                  {...item}
+                />
+              );
+            })}
+            {/* Mobile Accordion ends */}
           </div>
         </div>
       </div>
     </section>
   );
 };
+
+function ScheduleCard({ image, time, talks }) {
+  return (
+    <div className="row bt-bottom align-items-center pt-4 pb-4">
+      <div className="col-md-2">
+        <p className="mb-0 date-announced">{time}</p>
+      </div>
+      <div className="col-md-1 text-center">
+        <img src={image} className="img-fluid" alt="" />
+      </div>
+      <div className="col-md-9">
+        <div className="row">
+          {talks.map((talk, id) => {
+            const talkLength = talks.length;
+            return (
+              <ScheduleTalk
+                {...talk}
+                size={Math.floor(12 / talkLength)}
+                key={id}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ScheduleTalk({ description, speaker, track, size, proposalLink }) {
+  return (
+    <>
+      <div className={`col-${size} ${size == 1 || "text-center"}`}>
+        <p className="mb-0 date-content">
+          <a href={proposalLink} target="_blank" rel="noopener noreferrer">
+            {description}
+          </a>
+          {speaker && <span className="ft-weight"> <br /> {speaker} </span>}
+          {/* {track && <span className="rt-green text-white">{track}</span>} */}
+        </p>
+      </div>
+    </>
+  );
+}
+
+function ScheduleAccordion({ date, currentSchedule, id, handleTabClick }) {
+  return (
+    <Accordion key={id} className="d-block d-lg-none">
+      <Accordion.Item eventKey={id} onClick={() => handleTabClick(id)}>
+        <Accordion.Header>{date}</Accordion.Header>
+        <Accordion.Body style={{padding: "1rem 0rem"}}>
+          {currentSchedule.schedule.map((scheduleItem, idx) => {
+            return scheduleItem.talks.map(talk => {
+              return (
+                <Card style={{ margin: '0.8rem 0' }} key={idx}>
+                  <Card.Body>
+                    <Card.Title>
+                      <a href={talk.proposalLink} target="_blank" rel="noopener noreferrer">
+                        {talk.description}
+                      </a>
+                      <br />
+                      {talk.speaker && <>
+                        <span> By {talk.speaker} </span>
+                      </>}
+                    </Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{scheduleItem.time}</Card.Subtitle>
+                    {/* <Card.Text>
+                      {talk.track}
+                    </Card.Text> */}
+                  </Card.Body>
+                </Card>
+                // <div
+                //   className="row bt-bottom align-items-center pt-4 pb-4"
+                //   key={idx}
+                // >
+                //   <div className="col-md-3 col-5">
+                //     <p className="mb-0 date-announced">{scheduleItem.time}</p>
+                //   </div>
+                //   <div className="col-md-2 col-2 text-center">
+                //     <img src="images/icons/tab1.png" className="img-fluid" />
+                //   </div>
+
+                //   <div className="col-md-7 col-5" key={idx}>
+                //     <p className="mb-0 date-content">{talk.description}</p>
+                //   </div>
+                // </div>
+              );
+            });
+          })}
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
 
 export default ConferenceSchedule;
