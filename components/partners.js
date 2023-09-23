b5840d1355e76940e4dadb522a0302dcb149ef70
Bharat Saraswat <bsaraswat45@gmail.com>
2023-09-23 22:18:32 +0530

    update partners, add yashoda as medical partner (#212)
    * update partners, add yashoda as medical partner
    
    * add style changes for partners

--- components/partners.js
+++ components/partners.js
@@ -6,24 +6,36 @@ const Partners = () => {
     <section className="home-section" id="partners">
       <div className="container pt-5 pb-5">
         <div className="row align-items-center">
-          <h2 className="col-auto com-head" data-aos="fade-down" data-aos-duration="1000">Partners</h2>
+          <h2
+            className="col-auto com-head"
+            data-aos="fade-down"
+            data-aos-duration="1000"
+          >
+            Partners
+          </h2>
         </div>
         <div className="row justify-content-center mt-3 py-5">
           {PartnersData.map((partner, index) => (
-            <div className="col-md-3 col-6 bt-circle my-2 text-center" key={index}>
+            <div
+              className="col-md-3 col-6 bt-circle my-2 text-center"
+              key={index}
+            >
               <a href={partner.website} target="_blank" rel="noreferrer">
-              <img
-                src={partner.logo}
-                className={`img-fluid partners-logo ${partner.paddingClass}`}
-                alt={partner.name}
-              />
+                <img
+                  src={partner.logo}
+                  className={`img-fluid shadow-sm partners-logo ${partner.paddingClass}`}
+                  alt={partner.name}
+                />
               </a>
-              <p className="pt-2">{partner.name} <br/> ({partner.title})</p>
+              <p className="pt-2">
+                {partner.name} <br />(
+                <span className="partner-title">{partner.title}</span>)
+              </p>
             </div>
           ))}
         </div>
       </div>
-    </section >
+    </section>
   );
 };
 
