627b05fda940b1fb35bd776ee162d7fdd43824ca
Rajat Rajdeep <46029666+RajatRajdeep@users.noreply.github.com>
2023-09-24 14:42:45 +0530

    Fix: remove home section extra margin, ticket condition, open in new tab condition for nested nav items. (#216)

--- components/pricetable.js
+++ components/pricetable.js
@@ -3,6 +3,28 @@ import { useState, useEffect } from "react";
 import Button from "./button";
 import Paragraph from "./paragraph";
 
+const isTicketSoldOut = (ticket) => {
+  var soldOut = false;
+  if (ticket.end_timestamp) {
+    const parts = ticket.end_timestamp.split(' ');
+    const datePart = parts[0];
+    const timePart = parts[1];
+    const [year, month, day] = datePart.split('-').map(Number);
+    const [hours, minutes, seconds] = timePart.split(':').map(Number);
+
+    const ticketEndDate = new Date(Date.UTC(year, month - 1, day, hours, minutes, seconds));
+    // Adjust the date based on the IST offset
+    ticketEndDate.setHours(ticketEndDate.getHours() + 5);
+    ticketEndDate.setMinutes(ticketEndDate.getMinutes() + 30);
+    // Get the current date and time in IST
+    const currentDate = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
+    soldOut = ticketEndDate < currentDate;
+  }
+  soldOut = soldOut || (ticket.sold_out && !ticket.waitlist_enabled)
+    || (ticket.remaining_count <= 0 && !ticket.waitlist_enabled);
+  return soldOut;
+}
+
 const TicketsPriceTable = () => {
   const [tickets, setTickets] = useState([]);
 
@@ -18,7 +40,7 @@ const TicketsPriceTable = () => {
           id: ticket.ticket_id,
           name: ticket.ticket_name,
           price: ticket.minimum_price || ticket.ticket_price,
-          soldOut: (ticket.sold_out && !ticket.waitlist_enabled) || (ticket.remaining_count <= 0 && !ticket.waitlist_enabled),
+          soldOut: isTicketSoldOut(ticket),
           description: ticket.description,
         }));
         setTickets(extractedTickets);
