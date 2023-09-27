9dde8e306a7dfde1d817a8b0baea5cea6ca1b443
bhansa <bsaraswat45@gmail.com>
2023-09-27 13:29:52 +0530

    add sitemap post build script to generate sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://in.pycon.org/2023',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 400,
+  changefreq: "hourly",
+  priority: 0.8
+}
